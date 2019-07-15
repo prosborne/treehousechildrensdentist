716cd9f3d4c54c809b6a9d48c5b7bcc2c69e845c
Peter Riley Osborne <p.rileyosborne@gmail.com>
2019-07-15 08:35:31 -0700

    Up[dated initial build

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import { ThemeProvider } from 'styled-components';
+
+
+
+class Index extends Component {
+    render() {
+        return (
+            <ThemeProvider>
+
+            </ThemeProvider>
+        );
+    }
+}
+
+export default Index;
